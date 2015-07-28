5d4e583db4af93c66cf6337a5d3c44f8ec73afd3
zorfmorf <sebastian.tauch@aborium.com>
2015-07-28 10:40:07 +0200

    Implemented deletion of current folder before newgame/loading

--- src/misc/saveHandler.lua
+++ src/misc/saveHandler.lua
@@ -3,10 +3,27 @@
 saveHandler = {}
 
 
+-- try to delete folder at target path
+local function deleteFolderContents(path)
+    if love.filesystem.isFile(path) then 
+        log:msg("error", "Tried to delete folder that is a file:", path)
+        return
+    end
+    
+    local files = love.filesystem.getDirectoryItems(path)
+    for i,filename in ipairs(files) do
+        local dir = love.filesystem.getRealDirectory(path..filename)
+        os.remove(dir.."/"..path..filename)
+    end
+end
+
+
 -- low level method to copy files from to
 local function copyFiles(from, to)
     local ok = love.filesystem.createDirectory( to )
     if not ok then log:msg("verbose", "Error creating folder", to) end
+
+    -- gets all files in subdirectory from of both game.zip and save dir!
     local files = love.filesystem.getDirectoryItems( from )
     log:msg("verbose", "Copying", table.getn(files), "files from", from, "to", to)
     for i,item in ipairs(files) do
@@ -26,6 +43,7 @@ end
 
 -- copy master to current
 function saveHandler.newGame()
+    deleteFolderContents(C_MAP_CURRENT)
     copyFiles(C_MAP_MASTER, C_MAP_CURRENT)
     var.new()
 end
@@ -43,6 +61,7 @@ end
 
 -- load a game
 function saveHandler.loadGame()
+    deleteFolderContents(C_MAP_CURRENT)
     copyFiles(C_MAP_SAVEGAMES..'001/', C_MAP_CURRENT)
     entityHandler.load()
     var.load()
