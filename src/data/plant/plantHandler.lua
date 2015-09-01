9237bad05c8a79cf135c5cb871dac4105f6b397f
zorfmorf <sebastian.tauch@aborium.com>
2015-09-01 10:51:19 +0200

    Fixed bug with saving plant file

--- src/data/plant/plantHandler.lua
+++ src/data/plant/plantHandler.lua
@@ -4,28 +4,30 @@ plantHandler = {}
 
 function plantHandler.load()
     game.plants = {}
-    local file = love.filesystem.newFile( C_MAP_CURRENT..C_MAP_PLANTS )
-    file:open("r")
-    for line in file:lines( ) do
-        local values = line:split(";")
-        if #values == 6 then
-            local plant = nil
-            if values[1] == "Potatoe" then plant = Potatoe() end
-            plant.map = values[2]
-            plant.tx = tonumber(values[3])
-            plant.ty = tonumber(values[4])
-            plant.days = tonumber(values[5])
-            plant.state = tonumber(values[6])
-            table.insert(game.plants, plant)
+    if love.filesystem.isFile( C_MAP_CURRENT..C_MAP_PLANTS )then
+        local file = love.filesystem.newFile( C_MAP_CURRENT..C_MAP_PLANTS )
+        file:open("r")
+        for line in file:lines( ) do
+            local values = line:split(";")
+            if #values == 6 then
+                local plant = nil
+                if values[1] == "Potatoe" then plant = Potatoe() end
+                plant.map = values[2]
+                plant.tx = tonumber(values[3])
+                plant.ty = tonumber(values[4])
+                plant.days = tonumber(values[5])
+                plant.state = tonumber(values[6])
+                table.insert(game.plants, plant)
+            end
         end
+        file:close()
     end
-    file:close()
 end
 
 
 function plantHandler.save()
-    if love.filesystem.isFile( C_MAP_CURRENT..C_MAP_PLANTS )then
-        local file = love.filesystem.newFile( C_MAP_CURRENT..C_MAP_PLANTS )
+    local file = love.filesystem.newFile( C_MAP_CURRENT..C_MAP_PLANTS )
+    if file then 
         file:open("w")
         for i,plant in pairs(game.plants) do
             file:write(plant.type..';')
@@ -36,5 +38,7 @@ function plantHandler.save()
             file:write(tostring(plant.state)..'\n')
         end
         file:close()
+    else
+        log:msg("error", "Failed to create file", C_MAP_PLANTS)
     end
 end
